0e95c7c867051990bfe419df328ccd17bc1a1f5c
p1731016 <borseprathmesh89@gmail.com>
2025-02-26 16:52:34 +0530

    Add files via upload

---
+++ pythonlearning.py
@@ -0,0 +1,39 @@
+# print("Namaste Bharat")
+# print("prathmesh")
+
+# alphabet= 'prathmesh'
+# alph= "Abhishek"                #double us
+# alpha= '''Krishna'''                  #triple used for paragraph writing and long texts
+
+# a=1
+# b=2.4
+# c=3.56                             #to comment out any code use crtl + /
+
+# flag = True
+# prathmesh = False
+
+# print(alphabet,alph)          #this will print both word in the same line 
+# print(alpha)                  #as alpha is written in different line
+
+#in vs code 
+# # green- class  yellow-function   blue-variable
+
+# #print- prints 
+# #input- takes input like cin in c++
+# #type- determines the type of the function
+
+
+#age=input("Enter your age: ")              #here the input is in string form
+#age= int(age)                             #now here we are typecasting from string to integer
+#if age>=18:                                      #we have to first specify int or string so we have to typecast
+   # print("You are an Adult")
+#else:
+   # print("You are not an adult")
+
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+
